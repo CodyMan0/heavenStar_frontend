a06f2a5a664d2c1cdd330fa3058f1f84d4d70baf
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-02-27 11:21:53 +0900

    feat: react-router-dom을 활용한 Routing 설정 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/homePage";
+import ProfilePage from "scenes/homePage";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <LoginPage />,
+	},
+	{
+		path: "/home",
+		element: <HomePage />,
+	},
+	{
+		path: "/profile/:userId",
+		element: <ProfilePage />,
+	},
+]);
+
 function App() {
-	return <div className="App">hi</div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
