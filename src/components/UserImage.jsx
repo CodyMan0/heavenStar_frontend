74ee14c6e90c0b33d9c09bfe2cd1f5196fbe4135
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-03-05 10:58:59 +0900

    fix : 백앤드 포트 변경으로 인한 환경 변수 설정 변경

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -1,13 +1,14 @@
 import { Box } from '@mui/material';
 
 const UserImage = ({ image, size = '60px' }) => {
+  const URL = process.env.REACT_APP_BASE_URL;
   return (
     <Box width={size} height={size}>
       <img
         style={{ objectFit: 'cover', borderRadius: '50%' }}
         width={size}
         height={size}
-        src={`http://localhost:3001/assets/${image}`}
+        src={`${URL}/assets/${image}`}
         alt="user"
       />
     </Box>
--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -56,7 +56,7 @@ const PostWidget = ({
         subtitle={location}
         userPicturePath={userPicturePath}
       />
-      <Typography color={main} sx={{ mt: '1rem' }}>
+      <Typography color={main} sx={{ mt: '1rem' }}> 
         {description}
       </Typography>
       {picturePath && (
